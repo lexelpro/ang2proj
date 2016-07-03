4040ce0dfae39df74dfc6015007ebeb36327dacb
Alex Brykalov <alex.brykalov@gmail.com>
2016-07-03 22:38:48 +0300

    add properties to page

--- app/app.component.js
+++ app/app.component.js
@@ -11,11 +11,18 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'Ultra Racing';
+        this.carPart = {
+            "id": 1,
+            "name": "Super Tires",
+            "description": "These tires are the very best",
+            "inStock": 5
+        };
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>My SECOND Angular 2 App</h1>'
+            template: "<h1>{{title}}</h1>\n               <h2>{{carPart.name}}</h2>\n               <p>{{carPart.description}}</p>\n               <p>{{carPart.inStock}} in Stock</p>"
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
